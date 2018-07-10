9bab0c97ae1278f1ec33af39e91bf25cdd7ad80e
Bruno Bonanno <241804+bbonanno@users.noreply.github.com>
2018-07-10 18:22:08 +0100

    Add function0 matchers

--- src/main/scala/org/mockito/matchers/FunctionMatchers.scala
+++ src/main/scala/org/mockito/matchers/FunctionMatchers.scala
@@ -9,31 +9,17 @@
  * THIS SOFTWARE IS LICENSED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE AND ANY WARRANTY OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THIS SOFTWARE MAY BE REDISTRIBUTED TO OTHERS ONLY BY EFFECTIVELY USING THIS OR ANOTHER EQUIVALENT DISCLAIMER IN ADDITION TO ANY OTHER REQUIRED LICENSE TERMS.
  */
 
-package org.mockito
+package org.mockito.matchers
 
-import org.mockito.matchers._
+import org.mockito.{ArgumentMatcher, ArgumentMatchers => JavaMatchers}
 
-/**
- * Trait that provides some syntax sugar and type mapping.
- *
- * It mostly forwards the calls to org.mockito.ArgumentMatchers, but with a few improvements to make it more scala-like
- * It also renames the "eq" matcher to "eqTo" as in Scala "eq" is a keyword used to do object identity equality
- *
- * @author Bruno Bonanno
- *
- */
-trait ArgumentMatchersSugar
-    extends AnyMatchers
-    with EqMatchers
-    with ThatMatchers
-    with StringThatMatchers
-    with NullMatchers
-      with FunctionMatchers
+private[mockito] trait FunctionMatchers {
 
-/**
- * Simple object to allow the usage of the trait without mixing it in
- *
- * @author Bruno Bonanno
- *
- */
-object ArgumentMatchersSugar extends ArgumentMatchersSugar
+  def function0[T](value: T): () => T = JavaMatchers.argThat(new ArgumentMatcher[() => T] {
+
+    override def matches(argument: () => T): Boolean = argument() == value
+
+    override def toString: String = s"() => $value"
+  })
+
+}

a30c84e5317604b2b297cd14042391a9185ed88b
Jiro Kugiya <j.kugiya@gmail.com>
2018-09-03 15:33:45 +0900

    Don't check unused or unexpected stubs for lenient mocks

--- core/src/main/scala/org/mockito/MockitoScalaSession.scala
+++ core/src/main/scala/org/mockito/MockitoScalaSession.scala
@@ -1,20 +1,20 @@
 package org.mockito
 
-import org.mockito.MockitoScalaSession.{MockitoScalaSessionListener, UnexpectedInvocations}
-import org.mockito.exceptions.misusing.{UnexpectedInvocationException, UnnecessaryStubbingException}
+import org.mockito.MockitoScalaSession.{ MockitoScalaSessionListener, UnexpectedInvocations }
+import org.mockito.exceptions.misusing.{ UnexpectedInvocationException, UnnecessaryStubbingException }
 import org.mockito.internal.stubbing.StubbedInvocationMatcher
-import org.mockito.invocation.{DescribedInvocation, Invocation, Location}
+import org.mockito.invocation.{ DescribedInvocation, Invocation, Location }
 import org.mockito.listeners.MockCreationListener
 import org.mockito.mock.MockCreationSettings
 import org.mockito.quality.Strictness
 import org.mockito.quality.Strictness.STRICT_STUBS
 import org.mockito.session.MockitoSessionLogger
 
-import scala.collection.mutable
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 class MockitoScalaSession(name: String, strictness: Strictness, logger: MockitoSessionLogger) {
-  private val listener       = new MockitoScalaSessionListener
+  private val listener       = new MockitoScalaSessionListener(strictness)
   private val mockitoSession = Mockito.mockitoSession().name(name).logger(logger).strictness(strictness).startMocking()
 
   Mockito.framework().addListener(listener)
@@ -110,7 +110,7 @@ object MockitoScalaSession {
     def report(): Unit = if (nonEmpty) throw new UnnecessaryStubbingException(toString)
   }
 
-  class MockitoScalaSessionListener extends MockCreationListener {
+  class MockitoScalaSessionListener(strictness: Strictness) extends MockCreationListener {
     def reportIssues(): Seq[Reporter] = {
       val mockDetails = mocks.toSet.map(MockitoSugar.mockingDetails)
 
@@ -138,7 +138,7 @@ object MockitoScalaSession {
     private val mocks = mutable.Set.empty[AnyRef]
 
     override def onMockCreated(mock: AnyRef, settings: MockCreationSettings[_]): Unit =
-      if (!settings.isLenient) mocks += mock
+      if (!settings.isLenient && strictness != Strictness.LENIENT) mocks += mock
   }
 }
 

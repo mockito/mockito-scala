2e6a07b5067a08a8667efe9269737d035c36664e
Bruno Bonanno <241804+bbonanno@users.noreply.github.com>
2019-02-04 17:35:58 -0300

    Spies implement the same features as mocks (when it makes sense)

--- core/src/test/scala/user/org/mockito/MockitoSugarTest.scala
+++ core/src/test/scala/user/org/mockito/MockitoSugarTest.scala
@@ -2,49 +2,219 @@ package user.org.mockito
 
 import org.mockito.captor.ArgCaptor
 import org.mockito.invocation.InvocationOnMock
-import org.mockito.stubbing.{CallsRealMethods, DefaultAnswer, ScalaFirstStubbing, ScalaOngoingStubbing}
-import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
-import org.scalatest.{EitherValues, Matchers, OptionValues, WordSpec}
+import org.mockito.stubbing.{ CallsRealMethods, DefaultAnswer, ScalaFirstStubbing }
+import org.mockito.{ ArgumentMatchersSugar, MockitoSugar }
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{ EitherValues, Matchers, OptionValues, WordSpec }
 import user.org.mockito.matchers.ValueCaseClass
 
 //noinspection RedundantDefaultArgument
-class MockitoSugarTest extends WordSpec with MockitoSugar with Matchers with ArgumentMatchersSugar with EitherValues with OptionValues {
+class MockitoSugarTest
+    extends WordSpec
+    with MockitoSugar
+    with Matchers
+    with ArgumentMatchersSugar
+    with EitherValues
+    with OptionValues
+    with TableDrivenPropertyChecks {
 
-  class Foo {
-    def bar = "not mocked"
+  val scenarios = Table(
+    ("testDouble", "foo", "higherKinded", "concreteHigherKinded", "fooWithBaz", "baz"),
+    ("mock", () => mock[Foo], () => mock[HigherKinded[Option]], () => mock[ConcreteHigherKinded], () => mock[FooWithBaz], () => mock[Baz]),
+    ("spy",
+     () => spy(new Foo),
+     () => spy(new HigherKinded[Option]),
+     () => spy(new ConcreteHigherKinded),
+     () => spy(new FooWithBaz),
+     () => spy(new ConcreteBaz))
+  )
 
-    def iHaveSomeDefaultArguments(noDefault: String, default: String = "default value"): String = ???
+  forAll(scenarios) { (testDouble, foo, higherKinded, concreteHigherKinded, fooWithBaz, baz) =>
+    testDouble should {
+      "create a valid mock" in {
+        val aMock = foo()
 
-    def iStartWithByNameArgs(byName: => String, normal: String): String = ???
+        when(aMock.bar) thenReturn "mocked!"
 
-    def iHavePrimitiveByNameArgs(byName: => Int, normal: String): String = ???
+        aMock.bar shouldBe "mocked!"
+      }
 
-    def iHaveFunction0Args(normal: String, f0: () => String): String = ???
+      "stub a value class return value" in {
+        val aMock = foo()
 
-    def returnBar: Bar = ???
+        when(aMock.returnsValueCaseClass) thenReturn ValueCaseClass(100) andThen ValueCaseClass(200)
 
-    def doSomethingWithThisIntAndString(v: Int, v2: String): ValueCaseClass = ???
+        aMock.returnsValueCaseClass shouldBe ValueCaseClass(100)
+        aMock.returnsValueCaseClass shouldBe ValueCaseClass(200)
+      }
 
-    def returnsValueCaseClass: ValueCaseClass = ???
+      "create a mock with nice answer API (multiple params)" in {
+        val aMock = foo()
 
-    def returnsValueCaseClass(i: Int): ValueCaseClass = ???
+        when(aMock.doSomethingWithThisIntAndString(*, *)) thenAnswer ((i: Int, s: String) => ValueCaseClass(i * 10 + s.toInt)) andThenAnswer (
+            (i: Int,
+             _: String) => ValueCaseClass(i))
+
+        aMock.doSomethingWithThisIntAndString(4, "2") shouldBe ValueCaseClass(42)
+        aMock.doSomethingWithThisIntAndString(4, "2") shouldBe ValueCaseClass(4)
+      }
+
+      "create a mock while stubbing another" in {
+        val aMock = foo()
+
+        when(aMock.returnBar) thenReturn mock[Bar]
+
+        aMock.returnBar shouldBe a[Bar]
+      }
+
+      "default answer should deal with default arguments" in {
+        val aMock = foo()
+
+        aMock.iHaveSomeDefaultArguments("I'm not gonna pass the second argument")
+        aMock.iHaveSomeDefaultArguments("I'm gonna pass the second argument", "second argument")
+
+        verify(aMock).iHaveSomeDefaultArguments("I'm not gonna pass the second argument", "default value")
+        verify(aMock).iHaveSomeDefaultArguments("I'm gonna pass the second argument", "second argument")
+      }
+
+      "work with by-name arguments (argument order doesn't matter when not using matchers)" in {
+        val aMock = foo()
+
+        when(aMock.iStartWithByNameArgs("arg1", "arg2")) thenReturn "mocked!"
+
+        aMock.iStartWithByNameArgs("arg1", "arg2") shouldBe "mocked!"
+        aMock.iStartWithByNameArgs("arg111", "arg2") should not be "mocked!"
+
+        verify(aMock).iStartWithByNameArgs("arg1", "arg2")
+        verify(aMock).iStartWithByNameArgs("arg111", "arg2")
+      }
+
+      "work with primitive by-name arguments" in {
+        val aMock = foo()
+
+        when(aMock.iHavePrimitiveByNameArgs(1, "arg2")) thenReturn "mocked!"
+
+        aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
+        aMock.iHavePrimitiveByNameArgs(2, "arg2") should not be "mocked!"
+
+        verify(aMock).iHavePrimitiveByNameArgs(1, "arg2")
+        verify(aMock).iHavePrimitiveByNameArgs(2, "arg2")
+      }
+
+      "work with Function0 arguments" in {
+        val aMock = foo()
+
+        when(aMock.iHaveFunction0Args(eqTo("arg1"), function0("arg2"))) thenReturn "mocked!"
+
+        aMock.iHaveFunction0Args("arg1", () => "arg2") shouldBe "mocked!"
+        aMock.iHaveFunction0Args("arg1", () => "arg3") should not be "mocked!"
+
+        verify(aMock).iHaveFunction0Args(eqTo("arg1"), function0("arg2"))
+        verify(aMock).iHaveFunction0Args(eqTo("arg1"), function0("arg3"))
+      }
+
+      "reset" in {
+        val aMock = foo()
+
+        when(aMock.bar) thenReturn "mocked!"
+        when(aMock.iHavePrimitiveByNameArgs(1, "arg2")) thenReturn "mocked!"
+
+        aMock.bar shouldBe "mocked!"
+        aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
+
+        reset(aMock)
+
+        aMock.bar should not be "mocked!"
+        aMock.iHavePrimitiveByNameArgs(1, "arg2") should not be "mocked!"
+
+        //to verify the reset mock handler still handles by-name params
+        when(aMock.iHavePrimitiveByNameArgs(1, "arg2")) thenReturn "mocked!"
+
+        aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
+      }
+
+      "ignore the calls to the methods that provide default arguments" in {
+        val aMock = foo()
+
+        aMock.iHaveSomeDefaultArguments("I'm not gonna pass the second argument")
+
+        verify(aMock).iHaveSomeDefaultArguments("I'm not gonna pass the second argument", "default value")
+        verifyNoMoreInteractions(aMock)
+      }
+
+      "support an ArgCaptor and deal with default arguments" in {
+        val aMock = foo()
+
+        aMock.iHaveSomeDefaultArguments("I'm not gonna pass the second argument")
+
+        val captor1 = ArgCaptor[String]
+        val captor2 = ArgCaptor[String]
+        verify(aMock).iHaveSomeDefaultArguments(captor1, captor2)
+
+        captor1 hasCaptured "I'm not gonna pass the second argument"
+        captor2 hasCaptured "default value"
+      }
+
+      "work with parametrised return types" in {
+        val aMock = higherKinded()
+
+        when(aMock.method) thenReturn Some(Right("Mocked!"))
+
+        aMock.method.value.right.value shouldBe "Mocked!"
+      }
+
+      "work with parametrised return types declared in parents" in {
+        val aMock = concreteHigherKinded()
+
+        when(aMock.method) thenReturn Some(Right("Mocked!"))
+
+        aMock.method.value.right.value shouldBe "Mocked!"
+      }
+
+      "work with higher kinded types and auxiliary methods" in {
+        def whenGetById[F[_]](algebra: HigherKinded[F]): ScalaFirstStubbing[F[Either[String, String]]] =
+          when(algebra.method)
+
+        val aMock = higherKinded()
+
+        whenGetById(aMock) thenReturn Some(Right("Mocked!"))
+
+        aMock.method.value.right.value shouldBe "Mocked!"
+      }
+
+      "work with standard mixins" in {
+        val aMock = fooWithBaz()
+
+        when(aMock.bar) thenReturn "mocked!"
+        when(aMock.traitMethod(any)) thenReturn ValueCaseClass(69)
+
+        aMock.bar shouldBe "mocked!"
+        aMock.traitMethod(30) shouldBe ValueCaseClass(69)
+
+        verify(aMock).traitMethod(30)
+      }
+
+      "create a mock with nice answer API (single param)" in {
+        val aMock = baz()
+
+        when(aMock.traitMethod(*)) thenAnswer ((i: Int) => ValueCaseClass(i * 10 + 2)) andThenAnswer ((i: Int) =>
+          ValueCaseClass(i * 10 + 3))
+
+        aMock.traitMethod(4) shouldBe ValueCaseClass(42)
+        aMock.traitMethod(4) shouldBe ValueCaseClass(43)
+      }
+
+      "create a mock with nice answer API (invocation usage)" in {
+        val aMock = baz()
+
+        when(aMock.traitMethod(*)) thenAnswer ((i: InvocationOnMock) => ValueCaseClass(i.getArgument[Int](0) * 10 + 2)) andThenAnswer (
+            (i: InvocationOnMock) => ValueCaseClass(i.getArgument[Int](0) * 10 + 3))
+
+        aMock.traitMethod(4) shouldBe ValueCaseClass(42)
+        aMock.traitMethod(4) shouldBe ValueCaseClass(43)
+      }
+    }
   }
-
-  class Bar {
-    def iAlsoHaveSomeDefaultArguments(noDefault: String, default: String = "default value"): String = ???
-  }
-
-  trait Baz {
-    def traitMethod(arg: Int): ValueCaseClass = ???
-  }
-
-  class HigherKinded[F[_]] {
-    def method: F[Either[String, String]] = ???
-  }
-
-  class SomeClass extends Foo with Baz
-
-  class ConcreteHigherKinded extends HigherKinded[Option]
 
   "mock[T]" should {
     "work with type aliases" in {
@@ -57,102 +227,10 @@ class MockitoSugarTest extends WordSpec with MockitoSugar with Matchers with Arg
       aMock("Hola") shouldBe "Chau"
     }
 
-    "work with parametrised return types" in {
-      val aMock = mock[HigherKinded[Option]]
-
-      when(aMock.method) thenReturn Some(Right("Mocked!"))
-
-      aMock.method.value.right.value shouldBe "Mocked!"
-    }
-
-    "work with parametrised return types declared in parents" in {
-      val aMock = mock[ConcreteHigherKinded]
-
-      when(aMock.method) thenReturn Some(Right("Mocked!"))
-
-      aMock.method.value.right.value shouldBe "Mocked!"
-    }
-
-    "create a valid mock" in {
-      val aMock = mock[Foo]
-
-      when(aMock.bar) thenReturn "mocked!"
-
-      aMock.bar shouldBe "mocked!"
-    }
-
-    "stub a value class return value" in {
-      val aMock = mock[Foo]
-
-      when(aMock.returnsValueCaseClass) thenReturn ValueCaseClass(100) andThen ValueCaseClass(200)
-
-      aMock.returnsValueCaseClass shouldBe ValueCaseClass(100)
-      aMock.returnsValueCaseClass shouldBe ValueCaseClass(200)
-    }
-
-    "create a mock with nice answer API (single param)" in {
-      val aMock = mock[Baz]
-
-      when(aMock.traitMethod(*)) thenAnswer ((i: Int) => ValueCaseClass(i * 10 + 2)) andThenAnswer ((i: Int) => ValueCaseClass(i * 10 + 3))
-
-      aMock.traitMethod(4) shouldBe ValueCaseClass(42)
-      aMock.traitMethod(4) shouldBe ValueCaseClass(43)
-    }
-
-    "create a mock with nice answer API (invocation usage)" in {
-      val aMock = mock[Baz]
-
-      when(aMock.traitMethod(*)) thenAnswer ((i: InvocationOnMock) => ValueCaseClass(i.getArgument[Int](0) * 10 + 2)) andThenAnswer (
-          (i: InvocationOnMock) => ValueCaseClass(i.getArgument[Int](0) * 10 + 3))
-
-      aMock.traitMethod(4) shouldBe ValueCaseClass(42)
-      aMock.traitMethod(4) shouldBe ValueCaseClass(43)
-    }
-
-    "create a mock with nice answer API (multiple params)" in {
-      val aMock = mock[Foo]
-
-      when(aMock.doSomethingWithThisIntAndString(*, *)) thenAnswer ((i: Int, s: String) => ValueCaseClass(i * 10 + s.toInt)) andThenAnswer (
-          (i: Int,
-           _: String) => ValueCaseClass(i))
-
-      aMock.doSomethingWithThisIntAndString(4, "2") shouldBe ValueCaseClass(42)
-      aMock.doSomethingWithThisIntAndString(4, "2") shouldBe ValueCaseClass(4)
-    }
-
-    "create a mock while stubbing another" in {
-      val aMock = mock[Foo]
-
-      when(aMock.returnBar) thenReturn mock[Bar]
-
-      aMock.returnBar shouldBe a[Bar]
-    }
-
-    "default answer should deal with default arguments" in {
-      val aMock = mock[Foo]
-
-      aMock.iHaveSomeDefaultArguments("I'm not gonna pass the second argument")
-      aMock.iHaveSomeDefaultArguments("I'm gonna pass the second argument", "second argument")
-
-      verify(aMock).iHaveSomeDefaultArguments("I'm not gonna pass the second argument", "default value")
-      verify(aMock).iHaveSomeDefaultArguments("I'm gonna pass the second argument", "second argument")
-    }
-
     "create a mock with default answer" in {
       val aMock = mock[Foo](CallsRealMethods)
 
       aMock.bar shouldBe "not mocked"
-    }
-
-    "work with higher kinded types and auxiliary methods" in {
-      def whenGetById[F[_]](algebra: HigherKinded[F]): ScalaFirstStubbing[F[Either[String, String]]] =
-        when(algebra.method)
-
-      val aMock = mock[HigherKinded[Option]]
-
-      whenGetById(aMock) thenReturn Some(Right("Mocked!"))
-
-      aMock.method.value.right.value shouldBe "Mocked!"
     }
 
     "create a mock with default answer from implicit scope" in {
@@ -181,116 +259,10 @@ class MockitoSugarTest extends WordSpec with MockitoSugar with Matchers with Arg
       verify(aMock).traitMethod(30)
     }
 
-    "work with standard mixins" in {
-      val aMock = mock[SomeClass]
-
-      when(aMock.bar) thenReturn "mocked!"
-      when(aMock.traitMethod(any)) thenReturn ValueCaseClass(69)
-
-      aMock.bar shouldBe "mocked!"
-      aMock.traitMethod(30) shouldBe ValueCaseClass(69)
-
-      verify(aMock).traitMethod(30)
-    }
-
-    "work with by-name arguments (argument order doesn't matter when not using matchers)" in {
-      val aMock = mock[Foo]
-
-      when(aMock.iStartWithByNameArgs("arg1", "arg2")) thenReturn "mocked!"
-
-      aMock.iStartWithByNameArgs("arg1", "arg2") shouldBe "mocked!"
-      aMock.iStartWithByNameArgs("arg111", "arg2") shouldBe ""
-
-      verify(aMock).iStartWithByNameArgs("arg1", "arg2")
-      verify(aMock).iStartWithByNameArgs("arg111", "arg2")
-    }
-
-    "work with primitive by-name arguments" in {
-      val aMock = mock[Foo]
-
-      when(aMock.iHavePrimitiveByNameArgs(1, "arg2")) thenReturn "mocked!"
-
-      aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
-      aMock.iHavePrimitiveByNameArgs(2, "arg2") shouldBe ""
-
-      verify(aMock).iHavePrimitiveByNameArgs(1, "arg2")
-      verify(aMock).iHavePrimitiveByNameArgs(2, "arg2")
-    }
-
-    "work with Function0 arguments" in {
-      val aMock = mock[Foo]
-
-      when(aMock.iHaveFunction0Args(eqTo("arg1"), function0("arg2"))) thenReturn "mocked!"
-
-      aMock.iHaveFunction0Args("arg1", () => "arg2") shouldBe "mocked!"
-      aMock.iHaveFunction0Args("arg1", () => "arg3") shouldBe ""
-
-      verify(aMock).iHaveFunction0Args(eqTo("arg1"), function0("arg2"))
-      verify(aMock).iHaveFunction0Args(eqTo("arg1"), function0("arg3"))
-    }
-
     "should stop the user passing traits in the settings" in {
       a[IllegalArgumentException] should be thrownBy {
         mock[Foo](withSettings.extraInterfaces(classOf[Baz]))
       }
-    }
-  }
-
-  "reset[T]" should {
-    "reset mocks" in {
-      val aMock = mock[Foo]
-
-      when(aMock.bar) thenReturn "mocked!"
-      when(aMock.iHavePrimitiveByNameArgs(1, "arg2")) thenReturn "mocked!"
-
-      aMock.bar shouldBe "mocked!"
-      aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
-
-      reset(aMock)
-
-      aMock.bar shouldBe ""
-      aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe ""
-
-      //to verify the reset mock handler still handles by-name params
-      when(aMock.iHavePrimitiveByNameArgs(1, "arg2")) thenReturn "mocked!"
-
-      aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
-    }
-  }
-
-  "verifyNoMoreInteractions" should {
-    "ignore the calls to the methods that provide default arguments" in {
-      val aMock = mock[Foo]
-
-      aMock.iHaveSomeDefaultArguments("I'm not gonna pass the second argument")
-
-      verify(aMock).iHaveSomeDefaultArguments("I'm not gonna pass the second argument", "default value")
-      verifyNoMoreInteractions(aMock)
-    }
-  }
-
-  "argumentCaptor[T]" should {
-    "deal with default arguments" in {
-      val aMock = mock[Foo]
-
-      aMock.iHaveSomeDefaultArguments("I'm not gonna pass the second argument")
-
-      val captor1 = ArgCaptor[String]
-      val captor2 = ArgCaptor[String]
-      verify(aMock).iHaveSomeDefaultArguments(captor1, captor2)
-
-      captor1 hasCaptured "I'm not gonna pass the second argument"
-      captor2 hasCaptured "default value"
-    }
-  }
-
-  "spy[T]" should {
-    "create a valid spy" in {
-      val aSpy = spy(new Foo)
-
-      when(aSpy.bar) thenReturn "mocked!"
-
-      aSpy.bar shouldBe "mocked!"
     }
   }
 
---
+++ core/src/test/scala/user/org/mockito/TestModel.scala
@@ -0,0 +1,81 @@
+package user.org.mockito
+import user.org.mockito.matchers.{ ValueCaseClass, ValueClass }
+
+class Foo {
+  def bar = "not mocked"
+
+  def iHaveSomeDefaultArguments(noDefault: String, default: String = "default value"): String = "not mocked"
+
+  def iStartWithByNameArgs(byName: => String, normal: String): String = "not mocked"
+
+  def iHavePrimitiveByNameArgs(byName: => Int, normal: String): String = "not mocked"
+
+  def iHaveFunction0Args(normal: String, f0: () => String): String = "not mocked"
+
+  def returnBar: Bar = new Bar
+
+  def doSomethingWithThisIntAndString(v: Int, v2: String): ValueCaseClass = ValueCaseClass(v)
+
+  def returnsValueCaseClass: ValueCaseClass = ValueCaseClass(-1)
+
+  def returnsValueCaseClass(i: Int): ValueCaseClass = ValueCaseClass(i)
+}
+
+class Bar {
+  def iAlsoHaveSomeDefaultArguments(noDefault: String, default: String = "default value"): String = "not mocked"
+  def iHaveDefaultArgs(v: String = "default"): String                                             = "not mocked"
+}
+
+trait Baz {
+  def traitMethod(arg: Int): ValueCaseClass = ValueCaseClass(arg)
+}
+
+class ConcreteBaz extends Baz
+
+class HigherKinded[F[_]] {
+  def method: F[Either[String, String]] = null.asInstanceOf[F[Either[String, String]]]
+}
+
+class FooWithBaz extends Foo with Baz
+
+class ConcreteHigherKinded extends HigherKinded[Option]
+
+class Implicit[T]
+
+class Org {
+  def bar = "not mocked"
+  def baz = "not mocked"
+
+  def doSomethingWithThisInt(v: Int): Int = -1
+
+  def doSomethingWithThisIntAndString(v: Int, v2: String): String = "not mocked"
+
+  def doSomethingWithThisIntAndStringAndBoolean(v: Int, v2: String, v3: Boolean): String = "not mocked"
+
+  def returnBar: Bar = new Bar
+
+  def highOrderFunction(f: Int => String): String = "not mocked"
+
+  def iReturnAFunction(v: Int): Int => String = i => (i * v).toString
+
+  def iBlowUp(v: Int, v2: String): String = throw new IllegalArgumentException("I was called!")
+
+  def iHaveTypeParamsAndImplicits[A, B](a: A, b: B)(implicit v3: Implicit[A]): String = "not mocked"
+
+  def valueClass(n: Int, v: ValueClass): String = "not mocked"
+
+  def valueCaseClass(n: Int, v: ValueCaseClass): String = "not mocked"
+
+  def returnsValueCaseClass: ValueCaseClass = ValueCaseClass(-1)
+
+  def baz(i: Int, b: Baz2): String = "not mocked"
+
+  def fooWithVarArg(bells: String*): Unit                                              = ()
+  def fooWithSecondParameterList(bell: String)(cheese: Cheese): Unit                   = ()
+  def fooWithVarArgAndSecondParameterList(bells: String*)(cheese: Cheese): Unit        = ()
+  def valueClassWithVarArg(bread: Bread*): Unit                                        = ()
+  def valueClassWithSecondParameterList(bread: Bread)(cheese: Cheese): Unit            = ()
+  def valueClassWithVarArgAndSecondParameterList(breads: Bread*)(cheese: Cheese): Unit = ()
+}
+
+case class Baz2(param1: Int, param2: String)

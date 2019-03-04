745b5f4858cf3c49f31e64d938a82c34d5319ad9
Bruno Bonanno <bbonanno@gmail.com>
2019-03-04 19:09:49 +0000

    Prettify EqTo expected value

--- common/src/main/scala/org/mockito/matchers/EqTo.scala
+++ common/src/main/scala/org/mockito/matchers/EqTo.scala
@@ -2,8 +2,8 @@ package org.mockito.matchers
 
 import org.mockito.ArgumentMatcher
 import org.mockito.internal.ValueClassExtractor
-import org.scalactic.{ Equality, Prettifier }
 import org.scalactic.TripleEquals._
+import org.scalactic.{Equality, Prettifier}
 
 case class EqTo[T](value: T)(implicit $eq: Equality[T], $vce: ValueClassExtractor[T], $pt: Prettifier) extends ArgumentMatcher[T] {
   override def matches(argument: T): Boolean = value === argument || $vce.extractAs[T](value) === argument

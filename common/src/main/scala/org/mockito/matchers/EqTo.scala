78f0454ab2afc4b25403304e9ec3cf1f5b25d7be
Bruno Bonanno <bbonanno@gmail.com>
2019-05-05 23:05:21 +0100

    Improve EqTo code [ci skip]

--- common/src/main/scala/org/mockito/matchers/EqTo.scala
+++ common/src/main/scala/org/mockito/matchers/EqTo.scala
@@ -5,7 +5,10 @@ import org.mockito.internal.ValueClassExtractor
 import org.scalactic.TripleEquals._
 import org.scalactic.{ Equality, Prettifier }
 
-case class EqTo[T](value: T)(implicit $eq: Equality[T], $vce: ValueClassExtractor[T], $pt: Prettifier) extends ArgumentMatcher[T] {
-  override def matches(argument: T): Boolean = value === argument || $vce.extractAs[T](value) === argument
-  override def toString: String              = $pt(value)
+case class EqTo[T: Equality](value: T)(implicit $vce: ValueClassExtractor[T], $pt: Prettifier) extends ArgumentMatcher[T] {
+  private lazy val rawValue: T = $vce.extractAs[T](value)
+
+  override def matches(argument: T): Boolean = value === argument || rawValue === argument
+
+  override def toString: String = $pt(value)
 }

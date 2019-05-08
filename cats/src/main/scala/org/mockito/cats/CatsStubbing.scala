7666203f7878c00301dd4e6fbff811ea288107da
Bruno Bonanno <bbonanno@gmail.com>
2019-05-08 22:49:38 +0100

    Small code improvement

--- cats/src/main/scala/org/mockito/cats/CatsStubbing.scala
+++ cats/src/main/scala/org/mockito/cats/CatsStubbing.scala
@@ -6,9 +6,9 @@ import org.mockito.stubbing.OngoingStubbing
 
 case class CatsStubbing[F[_], T](delegate: OngoingStubbing[F[T]]) {
 
-  def thenReturn(value: T)(implicit a: Applicative[F]): CatsStubbing[F, T] = delegate.thenReturn(value.pure[F])
+  def thenReturn(value: T)(implicit a: Applicative[F]): CatsStubbing[F, T] = delegate thenReturn value.pure[F]
 
-  def thenFailWith[E](error: E)(implicit ae: ApplicativeError[F, E]): CatsStubbing[F, T] = delegate.thenReturn(error.raiseError[F, T])
+  def thenFailWith[E](error: E)(implicit ae: ApplicativeError[F, E]): CatsStubbing[F, T] = delegate thenReturn error.raiseError[F, T]
 
   def getMock[M]: M = delegate.getMock[M]
 }

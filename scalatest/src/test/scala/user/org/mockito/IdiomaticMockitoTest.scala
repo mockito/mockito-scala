07388b1aea967ae27f9d01fef8b393de4ecf61bf
Bruno Bonanno <bbonanno@gmail.com>
2019-09-22 18:29:01 +0100

    unify tests to cover spies too

--- scalatest/src/test/scala/user/org/mockito/IdiomaticMockitoTest.scala
+++ scalatest/src/test/scala/user/org/mockito/IdiomaticMockitoTest.scala
@@ -789,6 +789,91 @@ class IdiomaticMockitoTest extends WordSpec with Matchers with IdiomaticMockito 
 
         aMock.iHavePrimitiveByNameArgs(1, "arg2") shouldBe "mocked!"
       }
+
+      "correctly stub an invocation with concrete values" in {
+        val myService = orgDouble()
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) returns "hello3true"
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) shouldBe "hello3true"
+        myService.defaultParams("hello", defaultParam1 = true, defaultParam2 = 3) shouldBe "hello3true"
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) wasCalled twice
+      }
+
+      "correctly stub an invocation with matchers and concrete values" in {
+        val myService = orgDouble()
+
+        myService.defaultParams("hello", defaultParam2 = *, defaultParam1 = true) returns "hello3true"
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) shouldBe "hello3true"
+        myService.defaultParams("hello", defaultParam1 = true, defaultParam2 = 3) shouldBe "hello3true"
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) wasCalled twice
+      }
+
+      "correctly stub an invocation dependent values" in {
+        val myService = orgDouble()
+
+        myService.curriedDefaultParams("hello", defaultParam1 = true)() returns "hello3true"
+
+        myService.curriedDefaultParams("hello", defaultParam1 = true)() shouldBe "hello3true"
+
+        myService.curriedDefaultParams("hello", defaultParam1 = true)() was called
+      }
+
+      "correctly stub an invocation dependent values with default params applied" in {
+        val myService = orgDouble()
+
+        myService.curriedDefaultParams("hello")() returns "hello3true"
+
+        myService.curriedDefaultParams("hello")() shouldBe "hello3true"
+
+        myService.curriedDefaultParams("hello")() was called
+      }
+
+      "correctly doSomething an invocation with concrete values" in {
+        val myService = orgDouble()
+
+        "hello3true" willBe returned by myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true)
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) shouldBe "hello3true"
+        myService.defaultParams("hello", defaultParam1 = true, defaultParam2 = 3) shouldBe "hello3true"
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) wasCalled twice
+      }
+
+      "correctly do stub an invocation with matchers and concrete values" in {
+        val myService = orgDouble()
+
+        "hello3true" willBe returned by myService.defaultParams("hello", defaultParam2 = *, defaultParam1 = true)
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) shouldBe "hello3true"
+        myService.defaultParams("hello", defaultParam1 = true, defaultParam2 = 3) shouldBe "hello3true"
+
+        myService.defaultParams("hello", defaultParam2 = 3, defaultParam1 = true) wasCalled twice
+      }
+
+      "correctly do stub an invocation dependent values" in {
+        val myService = orgDouble()
+
+        "hello3true" willBe returned by myService.curriedDefaultParams("hello", defaultParam1 = true)()
+
+        myService.curriedDefaultParams("hello", defaultParam1 = true)() shouldBe "hello3true"
+
+        myService.curriedDefaultParams("hello", defaultParam1 = true)() was called
+      }
+
+      "correctly do stub an invocation dependent values with default params applied" in {
+        val myService = orgDouble()
+
+        "hello3true" willBe returned by myService.curriedDefaultParams("hello")()
+
+        myService.curriedDefaultParams("hello")() shouldBe "hello3true"
+
+        myService.curriedDefaultParams("hello")() was called
+      }
+
     }
   }
 

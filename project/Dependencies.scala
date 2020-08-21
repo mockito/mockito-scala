39bde26d1ef0bdabe2bf584a9f4eea3a308047bf
Bruno Bonanno <bbonanno@gmail.com>
2020-08-21 17:05:37 +0100

    Update Dependencies (#281)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object Dependencies {
 
-  val scalatestVersion = "3.2.0"
+  val scalatestVersion = "3.2.1"
 
   val commonLibraries = Seq(
-    "org.mockito"   % "mockito-core"      % "3.3.3",
+    "org.mockito"   % "mockito-core"      % "3.5.2",
     "org.scalactic" %% "scalactic"        % scalatestVersion,
-    "ru.vyarus"     % "generics-resolver" % "3.0.2",
+    "ru.vyarus"     % "generics-resolver" % "3.0.2"
   )
 
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.14.3"
@@ -15,7 +15,7 @@ object Dependencies {
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
 
   val specs2 = Seq(
-    "org.specs2"   %% "specs2-core"  % "4.10.0" % "provided",
+    "org.specs2"   %% "specs2-core"  % "4.10.3" % "provided",
     "org.hamcrest" % "hamcrest-core" % "2.2"   % "provided"
   )
 
@@ -25,5 +25,5 @@ object Dependencies {
   val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.2"   % "provided"
 
   val catsLaws = "org.typelevel" %% "cats-laws" % "2.0.0"
-  val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "1.0.1"
+  val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.0.0"
 }

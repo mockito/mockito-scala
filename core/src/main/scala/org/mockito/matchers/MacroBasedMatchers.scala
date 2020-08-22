a9084be7f8270f7710c41d5e6622b47d087e9a2d
Bruno Bonanno <bbonanno@gmail.com>
2020-08-22 16:42:29 +0100

    Implements #250

--- core/src/main/scala/org/mockito/matchers/MacroBasedMatchers.scala
+++ core/src/main/scala/org/mockito/matchers/MacroBasedMatchers.scala
@@ -1,5 +1,7 @@
 package org.mockito.matchers
 
+import org.mockito.{ ArgumentMatchers => JavaMatchers }
+
 trait MacroBasedMatchers {
 
   /**
@@ -7,7 +9,7 @@ trait MacroBasedMatchers {
    * but any now supports value classes so it is not needed anymore
    */
   @deprecated("Use 'any[T]' or '*[T]' instead", since = "1.0.2")
-  def anyVal[T](implicit $m: AnyMatcher[T]): T = $m.any
+  def anyVal[T](implicit default: DefaultValueProvider[T]): T = any[T]
 
   /**
    * Delegates to <code>ArgumentMatchers.any()</code>, it's main purpose is to remove the () out of
@@ -20,10 +22,13 @@ trait MacroBasedMatchers {
    *
    * It also fixes the NullPointerException when used on an value class argument (IMPORTANT: YOU MUST PROVIDE THE TYPE FOR VALUE CLASSES)
    */
-  def any[T](implicit $m: AnyMatcher[T]): T = $m.any
+  def any[T](implicit default: DefaultValueProvider[T]): T = {
+    JavaMatchers.any[T]()
+    default.default
+  }
 
   /**
    * Alias for [[org.mockito.matchers.MacroBasedMatchers.any]]
    */
-  def *[T](implicit $m: AnyMatcher[T]): T = $m.any
+  def *[T](implicit default: DefaultValueProvider[T]): T = any[T]
 }

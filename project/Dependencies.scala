ba9ef3103cb0670d6800c674c145cf9e9e1b32fd
Scala Steward <me@scala-steward.org>
2020-08-26 18:37:34 +0200

    Update discipline-scalatest to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,5 +25,5 @@ object Dependencies {
   val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.2"   % "provided"
 
   val catsLaws = "org.typelevel" %% "cats-laws" % "2.0.0"
-  val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.0.0"
+  val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.0.1"
 }

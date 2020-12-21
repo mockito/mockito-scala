486dbcb44e949ea8bedc27add6af844255f64e7f
Scala Steward <me@scala-steward.org>
2020-12-21 10:42:15 +0100

    Update scalacheck to 1.15.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     "ru.vyarus"     % "generics-resolver" % "3.0.3"
   )
 
-  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.1"
+  val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.2"
 
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
 

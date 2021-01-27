f748154c4b8ee42662eb579c9dd1c05ea89ebfd4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-27 23:25:54 +0100

    Update scalaz-core to 7.3.3 (#349)
    [ci skip]

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   def scalaReflection(scalaVersion: String) = "org.scala-lang" % "scala-reflect" % scalaVersion
 
   val cats   = "org.typelevel" %% "cats-core"   % "2.0.0" % "provided"
-  val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.2" % "provided"
+  val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.3" % "provided"
 
   val catsLaws            = "org.typelevel" %% "cats-laws"            % "2.0.0"
   val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.1.1"

34b6a800e8a67ce0afcbb47c7eb47a4a6bb6da92
Scala Steward <me@scala-steward.org>
2021-04-20 22:09:04 +0200

    Update scalatest to 3.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val scalatestVersion = "3.2.7"
+  val scalatestVersion = "3.2.8"
 
   val commonLibraries = Seq(
     "org.mockito"   % "mockito-core"      % "3.6.0",

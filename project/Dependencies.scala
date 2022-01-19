395c6c2bc59be1db522e1f9f132fbc380073f439
Scala Steward <me@scala-steward.org>
2022-01-19 23:20:32 +0100

    Update scalaz-core to 7.3.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,9 @@ object Dependencies {
   val scalatestVersion = "3.2.10"
 
   val commonLibraries = Seq(
-    "org.mockito"   % "mockito-core"      % "3.12.4",
-    "org.scalactic" %% "scalactic"        % scalatestVersion,
-    "ru.vyarus"     % "generics-resolver" % "3.0.3"
+    "org.mockito"    % "mockito-core"      % "3.12.4",
+    "org.scalactic" %% "scalactic"         % scalatestVersion,
+    "ru.vyarus"      % "generics-resolver" % "3.0.3"
   )
 
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.15.2"
@@ -15,14 +15,14 @@ object Dependencies {
   val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
 
   val specs2 = Seq(
-    "org.specs2"   %% "specs2-core"  % "4.10.6" % "provided",
-    "org.hamcrest" % "hamcrest-core" % "2.2"   % "provided"
+    "org.specs2"  %% "specs2-core"   % "4.10.6" % "provided",
+    "org.hamcrest" % "hamcrest-core" % "2.2"    % "provided"
   )
 
   def scalaReflection(scalaVersion: String) = "org.scala-lang" % "scala-reflect" % scalaVersion
 
   val cats   = "org.typelevel" %% "cats-core"   % "2.0.0" % "provided"
-  val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.5" % "provided"
+  val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.6" % "provided"
 
   val catsLaws            = "org.typelevel" %% "cats-laws"            % "2.0.0"
   val disciplineScalatest = "org.typelevel" %% "discipline-scalatest" % "2.1.1"

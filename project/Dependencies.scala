7263c0bd620a0f3035e5d3574bc7815aa309386b
Scala Steward <me@scala-steward.org>
2022-04-24 00:28:36 +0200

    Update mockito-core to 4.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val scalatestVersion = "3.2.11"
 
   val commonLibraries = Seq(
-    "org.mockito"    % "mockito-core"      % "4.3.1",
+    "org.mockito"    % "mockito-core"      % "4.5.1",
     "org.scalactic" %% "scalactic"         % scalatestVersion,
     "ru.vyarus"      % "generics-resolver" % "3.0.3"
   )

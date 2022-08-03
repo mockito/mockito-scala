42677fb1a6c335df4925927ee92f24fd0762288e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-03 10:29:50 +0200

    Update scalatest to 3.2.13 (#459)
    [skip ci]

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 
 object Dependencies {
 
-  val scalatestVersion = "3.2.12"
+  val scalatestVersion = "3.2.13"
 
   val commonLibraries = Seq(
     "org.mockito"    % "mockito-core"      % "4.6.1",

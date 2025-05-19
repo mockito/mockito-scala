c009fa40a0c86444f58687e013654bf33cf7c064
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-05-19 12:57:01 +0200

    Update hamcrest-core to 3.0 (#537)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   val specs2 = Seq(
     "org.specs2"  %% "specs2-core"   % "4.10.6" % "provided",
-    "org.hamcrest" % "hamcrest-core" % "2.2"    % "provided"
+    "org.hamcrest" % "hamcrest-core" % "3.0"    % "provided"
   )
 
   val scalaReflection = Def.setting(

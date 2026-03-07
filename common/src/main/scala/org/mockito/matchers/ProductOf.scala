d1b5bb4bffb6f33f156f34795541c8d867eda537
Eugene Platonov <jozic@live.com>
2026-03-07 16:34:09 -0500

    Prepare build infrastructure for Scala 3 support (#632)

--- common/src/main/scala/org/mockito/matchers/ProductOf.scala
+++ common/src/main/scala/org/mockito/matchers/ProductOf.scala
@@ -7,11 +7,6 @@ package matchers
 case class ProductOf[A, B] private (ma: ArgumentMatcher[A], mb: ArgumentMatcher[B]) extends ArgumentMatcher[(A, B)] {
   override def matches(ab: (A, B)): Boolean = ab match { case (a, b) => ma.matches(a) && mb.matches(b) }
   override def toString: String             = s"productOf($ma, $mb)"
-
-  // Address "-Xsource:3" warning
-  @deprecated("for bincompat only, do not use", "2.0.1")
-  private[mockito] def copy(ma: ArgumentMatcher[A] = this.ma, mb: ArgumentMatcher[B] = this.mb): ProductOf[A, B] =
-    new ProductOf(ma, mb)
 }
 
 object ProductOf {

89b639d6d8ad76a461e6802de5d746a6bd532093
Scala Steward <scala_steward@virtuslab.com>
2026-06-12 19:12:15 +0000

    Update scalaz-core to 7.3.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   )
 
   val cats   = "org.typelevel" %% "cats-core"   % "2.13.0" % "provided"
-  val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.8"  % "provided"
+  val scalaz = "org.scalaz"    %% "scalaz-core" % "7.3.9"  % "provided"
 
   val catsLaws = "org.typelevel" %% "cats-laws" % "2.13.0"
 
